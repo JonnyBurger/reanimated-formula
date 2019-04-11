6d3f87bcff9bee05e42c693be086c48162819669
Jonny Burger <jonathanburger11@gmail.com>
2019-04-11 20:12:03 +0200

    allow array

--- src/reduce-ast.ts
+++ src/reduce-ast.ts
@@ -16,7 +16,7 @@ const reduceAst = (
 	tree: ASTNode,
 	variables: Variables,
 	mathType: MathType
-): number | Animated.Node<number> => {
+): number | Animated.Node<number> | (number | Animated.Node<number>)[] => {
 	if (!tree) {
 		throw new InvalidExpressionError('Could not parse');
 	}
@@ -82,6 +82,27 @@ const reduceAst = (
 			}
 			return Math.sqrt(left as number);
 		}
+
+		if (tree.token.value === 'min') {
+			if (!Array.isArray(left) || left.length < 2) {
+				throw new TypeError(`${logPrefix} Must pass multiple values to min()`);
+			}
+			if (mathType === 'reanimated') {
+				// @ts-ignore
+				return Animated.min(...left);
+			}
+			return Math.min(...(left as number[]));
+		}
+		if (tree.token.value === 'max') {
+			if (!Array.isArray(left) || left.length < 2) {
+				throw new TypeError(`${logPrefix} Must pass multiple values to max()`);
+			}
+			if (mathType === 'reanimated') {
+				// @ts-ignore
+				return Animated.max(...left);
+			}
+			return Math.max(...(left as number[]));
+		}
 	}
 	const right = reduceAst(tree.right as ASTNode, variables, mathType);
 	if (tree.token.value === '+') {
@@ -110,6 +131,12 @@ const reduceAst = (
 			throw new TypeError(`${logPrefix} Cannot use operator "*" on array`);
 		}
 		return (left as number) * (right as number);
+	}
+	if (tree.token.value === ',') {
+		if (Array.isArray(left) || Array.isArray(right)) {
+			throw new TypeError(`${logPrefix} Cannot use operator "," on array`);
+		}
+		return [left, right];
 	}
 	throw new Error('Could not parse!');
 };
